869b4e721e8765ae0fd9f410f01e980ff69db145
Praisino <praisetaiwo@gmail.com>
2026-01-20 08:26:27 +0000

    add has_many association to Post

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :posts
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :posts do
+    resources :comments
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "posts#index"
 end
